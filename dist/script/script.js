36fa3234db39c0c87bafd1d8350c0f0624b5f42f
Rishi <rishiagrahari36@gmail.com>
2024-06-01 17:41:54 +0530

    update file

---
+++ dist/script/script.js
@@ -0,0 +1,15 @@
+// let logo = document.querySelectorAll(".img-box img")
+// let cards = document.querySelectorAll(".commit-options > ul")
+
+// let round = () => {
+//   logo.classList.add("rotation")
+// }
+// let stop = () => {
+//   logo.classList.remove("rotation")
+// }
+
+// for (let card of cards) {
+//   card.onmouseover = round;
+//   card.onmouseout = stop;
+// }
+
